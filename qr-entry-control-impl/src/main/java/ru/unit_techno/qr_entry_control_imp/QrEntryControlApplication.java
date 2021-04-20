01256f2d17fc595c164cd7c4a8d3ca6589504c25
Alexey Vasin <aevasin@vtb.ru>
2021-04-20 21:17:03 +0300

    init application carcass

---
+++ qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_imp/QrEntryControlApplication.java
@@ -0,0 +1,12 @@
+
+package ru.unit_techno.qr_entry_control_imp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrEntryControlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QrEntryControlApplication.class, args);
+    }
+}
