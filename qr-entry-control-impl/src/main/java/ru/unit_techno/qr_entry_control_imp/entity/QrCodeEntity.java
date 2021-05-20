6902199c536ff58211dccb003c3687f7bc83c6f1
Alexey Vasin <empty>
2021-05-20 22:06:01 +0300

    -refactoring entities; -add delivery status;

--- qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_imp/entity/QrCodeEntity.java
+++ qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_imp/entity/QrCodeEntity.java
@@ -2,124 +2,52 @@
 package ru.unit_techno.qr_entry_control_imp.entity;
 
 import lombok.Data;
+import ru.unit_techno.qr_entry_control_imp.entity.enums.DeliveryStatus;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.sql.Timestamp;
-import java.util.Objects;
 
 @Data
 @Entity
 @Table(name = "qr_code", schema = "qr_entry_control")
 @SequenceGenerator(name = "qr_code_id_seq", sequenceName = "qr_code_id_seq")
 public class QrCodeEntity {
-    private Long qrId;
-    private String qrPicture;
-    private Timestamp creationDate;
-    private String governmentNumber;
-    private String name;
-    private String surname;
-    private String email;
-    private Boolean expire;
-
     @Id
     @Column(name = "qr_id")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "qr_code_id_seq")
-    public Long getQrId() {
-        return qrId;
-    }
-
-    public void setQrId(Long qrId) {
-        this.qrId = qrId;
-    }
-
+    private Long qrId;
     @Basic
     @Column(name = "qr_picture")
-    public String getQrPicture() {
-        return qrPicture;
-    }
-
-    public void setQrPicture(String qrPicture) {
-        this.qrPicture = qrPicture;
-    }
-
+    private String qrPicture;
     @Basic
     @Column(name = "creation_date")
-    //TODO create date time after save
-    public Timestamp getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Timestamp creationDate) {
-        this.creationDate = creationDate;
-    }
-
+    private Timestamp creationDate;
     @Basic
     @Column(name = "government_number")
-    public String getGovernmentNumber() {
-        return governmentNumber;
-    }
-
-    public void setGovernmentNumber(String governmentNumber) {
-        this.governmentNumber = governmentNumber;
-    }
-
+    private String governmentNumber;
     @Basic
     @Column(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    private String name;
     @Basic
     @Column(name = "surname")
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    @Basic
-    @Column(name = "expire")
-    public Boolean getExpire() {
-        return expire;
-    }
-
-    public void setExpire(Boolean expire) {
-        this.expire = expire;
-    }
-
+    private String surname;
     @Basic
     @Column(name = "email")
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        QrCodeEntity that = (QrCodeEntity) o;
-        return Objects.equals(qrId, that.qrId) && Objects.equals(qrPicture, that.qrPicture) && Objects.equals(creationDate, that.creationDate) && Objects.equals(governmentNumber, that.governmentNumber) && Objects.equals(name, that.name) && Objects.equals(surname, that.surname) && Objects.equals(expire, that.expire);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(qrId, qrPicture, creationDate, governmentNumber, name, surname, expire);
-    }
+    private String email;
+    @Basic
+    @Column(name = "expire")
+    private Boolean expire;
+    @Basic
+    @Column(name = "delivery_status")
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
 }
