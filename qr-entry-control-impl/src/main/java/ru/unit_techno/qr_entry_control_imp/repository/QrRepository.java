4e660db60b8b4d249a8d210571d869d653723279
Pasha Soldatov <fastwizardr@gmail.com>
2021-05-31 20:36:54 +0300

    -Add scheduler job that expiring all QR codes then older 1 hour after entering date

--- qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_imp/repository/QrRepository.java
+++ qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_imp/repository/QrRepository.java
@@ -4,16 +4,13 @@ package ru.unit_techno.qr_entry_control_imp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 import ru.unit_techno.qr_entry_control_imp.entity.QrCodeEntity;
-
-import java.util.List;
 
 public interface QrRepository extends JpaRepository<QrCodeEntity, Long> {
 
     void deleteAllByExpireTrue();
 
     @Modifying
-    @Query(value = "update qr_code set expire = true where (now() - creation_date) > 2 * interval '1 day'", nativeQuery = true)
+    @Query(value = "update qr_code set expire = true where (entering_date - now()) < (- interval '1 hour')", nativeQuery = true)
     void expireOldQrCodes();
 }
