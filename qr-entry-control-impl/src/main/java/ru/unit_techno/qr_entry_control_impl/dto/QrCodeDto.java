30b4778c86d66c496c6ea926cfd29ecb070e6e38
Васин Алексей Евгеньевич <empty>
2021-09-23 22:36:17 +0300

    add validate govNumber

--- qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_impl/dto/QrCodeDto.java
+++ qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_impl/dto/QrCodeDto.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import ru.unit_techno.qr_entry_control_impl.serializer.TimestampDateSerializer;
 
+import javax.validation.constraints.Pattern;
 import java.sql.Timestamp;
 
 @Data
 public class QrCodeDto {
     private String email;
+    @Pattern(regexp = "^[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2} \\d{2,3}$")
     private String governmentNumber;
     private String name;
     private String surname;
