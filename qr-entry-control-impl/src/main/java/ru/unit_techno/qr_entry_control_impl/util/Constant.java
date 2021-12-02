84e45b163d0e6af8d46e077fcd5998115e56e22b
PavelSoldatov <akelloghbdtn17177>
2021-12-02 16:11:00 +0300

    -Fix QR temp directory path 2

--- qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_impl/util/Constant.java
+++ qr-entry-control-impl/src/main/java/ru/unit_techno/qr_entry_control_impl/util/Constant.java
@@ -7,6 +7,6 @@ import lombok.experimental.UtilityClass;
 public class Constant {
     public static final String REGEX = "(^[АВЕКМНОРСТУХ]\\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{4}(?<!0000)\\d{2,3})|(^\\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2}\\d{2,3})|(^[АВЕКМНОРСТУХ]{2}\\d{3}(?<!000)[АВЕКМНОРСТУХ]\\d{2,3})$";
     public static final String WORKDIR_PATH = System.getProperty("user.dir");
-    public static final String QR_TEMP_DIR = WORKDIR_PATH + "\\temp";
-    public static final String PATH_TO_QRS = QR_TEMP_DIR + "\\";
+    public static final String QR_TEMP_DIR = WORKDIR_PATH + "/qrs";
+    public static final String PATH_TO_QRS = QR_TEMP_DIR + "/";
 }
